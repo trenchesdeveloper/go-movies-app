70cc0f30a8a18db27e35f8bc54aed7c16253cc7a
trenchesdeveloper <opeyemisamuel222@gmail.com>
2023-12-25 14:21:28 +0100

    add errorJson helper?

--- go-movies-backend/cmd/api/utils.go
+++ go-movies-backend/cmd/api/utils.go
@@ -61,3 +61,18 @@ func (app *application) readJson(w http.ResponseWriter, r *http.Request, data in
 
 	return nil
 }
+
+func (app *application) errorJson(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload JsonResponse
+
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return app.writeJson(w, statusCode, payload)
+}
