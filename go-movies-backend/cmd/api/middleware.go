c10b0d0be3f173e371c0cfba200e2b34cab1bb82
trenchesdeveloper <opeyemisamuel222@gmail.com>
2023-12-25 09:29:59 +0100

    enable react proxy

--- go-movies-backend/cmd/api/middleware.go
+++ go-movies-backend/cmd/api/middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		if r.Method == "OPTIONS" {
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Accept, X-CSRF-Token, Authorization")
